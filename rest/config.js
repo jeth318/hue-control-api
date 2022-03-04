55f927cb58d2208d78ed3fac1d0ebbd0e32b0932
jeth318 <jesper.thornberg@me.com>
2022-03-04 17:16:18 +0100

    Update ip address and port

--- rest/config.js
+++ rest/config.js
@@ -3,10 +3,11 @@ const { config } = require('dotenv');
 config();
 
 const host = process.env.HUE_BRIDGE_IP;
+const hostPort = process.env.HUE_BRIDGE_PORT;
 const userId = process.env.HUE_USER_ID;
 const protocol = process.env.PROTOCOL;
 const port = process.env.PORT;
-const baseUrl = `${protocol}://${host}/api/${userId}`;
+const baseUrl = `${protocol}://${host}:${hostPort}/api/${userId}`;
 const endpoints = {
     LIGHTS: 'lights',
     GROUPS: 'groups',
