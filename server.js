079f17adf9c350452a0baaa975da0d9feae3d5af
jeth318 <jesper.thornberg@me.com>
2022-03-09 19:24:47 +0100

    Fix endpoint

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const { port } = require('./rest/config');
-const cors = require('cors');
-const bodyParser = require('body-parser');
+const { port } = require("./rest/config");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 // config should be imported before importing any other file
-const app = require('express')();
+const app = require("express")();
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-const router = require('./router');
-app.use("/lights/api", router);
+const router = require("./router");
+app.use("/hue", router);
 
 app.listen(port, () => {
   console.info(`server started on port ${port}`);
