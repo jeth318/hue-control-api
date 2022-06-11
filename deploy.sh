ac972709b0149c8f4370efd73bff63ece3211550
jeth318 <jesper.thornberg@me.com>
2022-06-11 17:42:55 +0200

    Update deploy

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+branch=$(git branch --show-current)
+
+if [[ $branch == "main" ]]; then
+    echo "On main branch. Will build and ship to server"
+    npm run build
+    echo "Shipping dist to server"
+    rsync -arvz -e 'ssh -p 2244' --progress $(pwd)/dist/* jeth@elara.local:/home/jeth/apps/home-control
+else
+    echo "Not on main branch. Skipping build and deploy."
+fi
