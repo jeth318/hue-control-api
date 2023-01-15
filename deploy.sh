21beb2923dcfc1f6629ec76bbece02df73927340
jeth318 <jesper.thornberg@me.com>
2023-01-15 21:59:23 +0100

    Revert to original deploy script

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,8 @@ if [[ $branch == "main" ]]; then
     echo "On main branch. Will build and ship to server"
     npm run build
     echo "Shipping dist to server"
-    #rsync -arvz -e 'ssh -p 2244' --progress $(pwd)/dist/* jeth@elara.local:/home/jeth/apps/home-control
-    scp -P 2244 $(pwd)/dist/* jeth@elara.local:/home/jeth/apps/home-control
+    rsync -arvz -e 'ssh -p 2244' --progress $(pwd)/dist/* jeth@elara.local:/home/jeth/apps/home-control
+    #scp -P 2244 $(pwd)/dist/* jeth@elara.local:/home/jeth/apps/home-control
 
 else
     echo "Not on main branch. Skipping build and deploy."
